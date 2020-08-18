35178cc48eb10a56b1264461c912ae239530fb38
deep-1995 <dhaliwalprabh93@gmail.com>
2020-08-18 13:51:51 -0700

    adding transaction .js code

--- models/transaction.js
+++ models/transaction.js
@@ -2,25 +2,22 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: "Enter a name for transaction"
-    },
-    value: {
-      type: Number,
-      required: "Enter an amount"
-    },
-    date: {
-      type: Date,
-      default: Date.now
-    }
-  }
-);
+const transactionSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction",
+  },
+  value: {
+    type: Number,
+    required: "Enter an amount",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
 module.exports = Transaction;
-© 2020 GitHub, Inc.

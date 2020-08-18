1d4bdb89b8330386ff08b8c60fad9bdcea24666b
deep-1995 <dhaliwalprabh93@gmail.com>
2020-08-18 13:45:58 -0700

    adding coding

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+//Dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+//Set up port so it can be deployed or used locally
 const PORT = process.env.PORT || 3000;
 
+//Use Express
 const app = express();
 
+//Middleware
 app.use(logger("dev"));
 
 app.use(compression());
@@ -15,6 +19,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//Set up connection to mogoDB for use in deployment or locally
 let workoutDb = process.env.MONGODB_URI || "mongodb://localhost/budget";
 mongoose
   .connect(workoutDb, { useNewUrlParser: true, useFindAndModify: false })
@@ -25,9 +30,10 @@ mongoose
     console.log(err);
   });
 
-// routes
+//Routes
 app.use(require("./routes/api.js"));
 
+//Port listening
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
